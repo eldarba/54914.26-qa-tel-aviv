11a11405027c9db55ce554f08145f25b9acd9fe8
jbt <jbt@403-ins>
2022-04-11 15:48:21 +0300

    AAA

--- java-1-eldar/src/c/random/App3.java
+++ java-1-eldar/src/c/random/App3.java
@@ -12,7 +12,11 @@ public class App3 {
 		System.out.print("enter 2nd number: ");
 		int b = sc.nextInt();
 		sc.close();
+		
 		System.out.println(a + " + " + b + " = " + (a + b));
+		System.out.println(a + " - " + b + " = " + (a - b));
+		System.out.println(a + " * " + b + " = " + (a * b));
+		System.out.println(a + " : " + b + " = " + (a / b));
 
 	}
 
