c607124cb9912aab285aa31c5c240314a9e46b1f
jbt <jbt@403-ins>
2022-04-11 12:45:03 +0300

    aaa

--- java-1-eldar/src/a/App5Operators.java
+++ java-1-eldar/src/a/App5Operators.java
@@ -9,7 +9,7 @@ public class App5Operators {
 		// expressions: 150 + 36
 		
 		// ARITHMETICAL OPERATORS 
-		System.out.println("ARITHMETICAL OPERATORS");
+		System.out.println("============ ARITHMETICAL OPERATORS");
 		System.out.println(5 + 3); // plus
 		System.out.println(5 - 3); // minus
 		System.out.println(5 * 3); // multiply
@@ -20,9 +20,23 @@ public class App5Operators {
 		System.out.println(5 / 3D); // to get a decimal result one of the operands must be decimal
 		
 		// LOGICAL OPERATORS
-		System.out.println("LOGICAL OPERATORS");
+		System.out.println("============ LOGICAL OPERATORS");
 		System.out.println(5 < 9);
+		System.out.println(5 > 9);
+		System.out.println(5 <= 9);
+		System.out.println(5 >= 9);
+		System.out.println(5 == 9);
+		System.out.println(5 != 9);
 		
+		System.out.println("============ COMBINE LOGICAL OPERATORS");
+		int minAge = 18;
+		int maxAge = 120;
+		int age = 35;
+		// AND - find out if age is legal
+		System.out.println(age >= minAge && age <= maxAge);
+		// OR
+		// AND - find out if age is illegal
+		System.out.println(age < minAge || age > maxAge);
 		
 		
 		
