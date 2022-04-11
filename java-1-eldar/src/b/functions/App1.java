3b475d5fa2e941e7ca9d51e4026b117cf91012ea
jbt <jbt@403-ins>
2022-04-11 14:43:30 +0300

    AAA

---
+++ java-1-eldar/src/b/functions/App1.java
@@ -0,0 +1,42 @@
+package b.functions;
+
+public class App1 {
+
+	public static void main(String[] args) {
+
+		int x; // variable declaration is done only once
+
+		// call the add method and get the result
+		x = add(5, 6);
+		// print
+		System.out.println(x);
+
+		// call the subtract method and get the result
+		x = subtract(100, 70);
+		// print
+		System.out.println(x);
+
+	}
+
+	// method definition
+	public static int add(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+
+	public static int subtract(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+
+	public static int multiply(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+
+	public static double divide(double a, double b) {
+		double result = a / b;
+		return result;
+	}
+
+}
