b7b5e34d7846552e6bcf4ca3f8492f764b670d29
jbt <jbt@403-ins>
2022-04-13 11:04:17 +0300

    AAA

---
+++ java-2-eldar/src/b/loops/Demo2.java
@@ -0,0 +1,17 @@
+package b.loops;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+
+		int c = 1;
+
+		while (c <= 100) {
+			System.out.println(c);
+			// c = c + 1;
+			c++;
+		}
+
+	}
+
+}
---
+++ java-2-eldar/src/b/loops/Demo3.java
@@ -0,0 +1,17 @@
+package b.loops;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+
+		int c = 2;
+
+		while (c <= 100) {
+			System.out.println(c);
+			// c = c + 2;
+			c += 2;
+		}
+
+	}
+
+}
