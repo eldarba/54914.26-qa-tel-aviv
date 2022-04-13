9ea2c922f76c1fb9d1f2d0b1804d1fe55e822028
jbt <jbt@403-ins>
2022-04-13 11:16:17 +0300

    AAA

---
+++ java-2-eldar/src/c/arrays/Demo1.java
@@ -0,0 +1,21 @@
+package c.arrays;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		// array of int (length 3)
+		int[] arr = new int[3];
+		// populate array with data
+		arr[0] = 7;
+		arr[1] = -9;
+		arr[2] = 11;
+		// access the array elements (for print)
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		// System.out.println(arr[3]); // out of array bounds
+
+	}
+
+}
