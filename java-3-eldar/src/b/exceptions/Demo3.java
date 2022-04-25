175dd8a85f3f50f354e508dad52d6777c649a059
jbt <jbt@403-ins>
2022-04-25 12:48:00 +0300

    AAA

--- java-3-eldar/src/b/exceptions/Demo3.java
+++ java-3-eldar/src/b/exceptions/Demo3.java
@@ -1,6 +1,7 @@
 package b.exceptions;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Demo3 {
 
@@ -13,6 +14,20 @@ public class Demo3 {
 			System.out.println("file exist");
 		} else {
 			System.out.println("file NOT exist");
+		}
+
+		try {
+			Scanner sc = new Scanner(file);
+		} catch (Exception e) {
+			// if we are here an error occurred - file not found
+			System.out.println("scanner error: file not found. creating the file");
+			try {
+				file.createNewFile(); // create the file if not exist
+				System.out.println("file created");
+			} catch (Exception e1) {
+				System.out.println("file create failed");
+			}
+
 		}
 
 	}
