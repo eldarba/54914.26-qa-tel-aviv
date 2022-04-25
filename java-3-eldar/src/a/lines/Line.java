0fb4a8c4ba7cce37660c18e1ac8eec004dc71925
jbt <jbt@403-ins>
2022-04-25 10:07:21 +0300

    AAA

--- java-3-eldar/src/a/lines/Line.java
+++ java-3-eldar/src/a/lines/Line.java
@@ -2,8 +2,8 @@ package a.lines;
 
 public class Line {
 
-	String color;
-	int length;
+	private String color;
+	private int length;
 
 	void print() {
 		System.out.println(color);
@@ -11,6 +11,17 @@ public class Line {
 			System.out.print("*");
 		}
 		System.out.println();
+	}
+
+	// access to attributes using get / set methods
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setLength(int length) {
+		if (length >= 0) {
+			this.length = length;
+		}
 	}
 
 }
--- java-3-eldar/src/a/lines/LineTest.java
+++ java-3-eldar/src/a/lines/LineTest.java
@@ -8,11 +8,19 @@ public class LineTest {
 		Line myLine = new Line();
 
 		// set color
-		myLine.color = "Red";
+		// myLine.color = "Red";
+		myLine.setColor("Red");
 		// set length
-		myLine.length = 7;
-
+		// myLine.length = -7;
+		myLine.setLength(5);
 		// use the print method
+		myLine.print();
+
+		// this call will NOT change the length
+		myLine.setLength(-5);
+		myLine.print();
+		// this call WILL change the length
+		myLine.setLength(3);
 		myLine.print();
 
 	}
